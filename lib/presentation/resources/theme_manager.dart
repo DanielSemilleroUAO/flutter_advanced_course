26ba576a0be4c6fa66e27f710cb1d451bd0a3e3d
Daniel Delgado Rodríguez <danieldelgado_2106@hotmail.com>
2022-03-25 17:26:02 -0500

    Upload project

---
+++ lib/presentation/resources/theme_manager.dart
@@ -0,0 +1,110 @@
+
+import 'package:complete_advanced_flutter/presentation/resources/color_manager.dart';
+import 'package:complete_advanced_flutter/presentation/resources/font_manager.dart';
+import 'package:complete_advanced_flutter/presentation/resources/styles_manager.dart';
+import 'package:complete_advanced_flutter/presentation/resources/values_manager.dart';
+import 'package:flutter/material.dart';
+
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    // mian colors app
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    // ripple color
+    splashColor: ColorManager.primaryOpacity70,
+    disabledColor: ColorManager.grey1, // will be used incase of disable button
+    accentColor: ColorManager.grey,
+    // card view them
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
+    //App bar theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle:
+          getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16),
+    ),
+    // Button theme
+    buttonTheme: ButtonThemeData(
+      shape: StadiumBorder(),
+      disabledColor: ColorManager.grey1,
+      buttonColor: ColorManager.primary,
+      splashColor: ColorManager.primaryOpacity70,
+    ),
+    // Eleveted button them
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: getRegularStyle(color: ColorManager.white),
+        primary: ColorManager.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSize.s12),
+        ),
+      ),
+    ),
+    // text theme
+    textTheme: TextTheme(
+      headline1: getSemiBoldStyle(
+          color: ColorManager.darkGrey, fontSize: FontSize.s16),
+      subtitle1:
+          getMediumStyle(color: ColorManager.lightGrey, fontSize: FontSize.s14),
+      caption: getRegularStyle(color: ColorManager.grey),
+      bodyText1: getRegularStyle(color: ColorManager.grey),
+    ),
+    //input decoration theme
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(
+        AppPadding.p8,
+      ),
+      hintStyle: getRegularStyle(color: ColorManager.grey1),
+      // label style
+      labelStyle: getMediumStyle(color: ColorManager.darkGrey),
+      // error style
+      errorStyle: getRegularStyle(color: ColorManager.error),
+      //enable border
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.grey,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s8),
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s8),
+        ),
+        borderSide: BorderSide(
+          color: ColorManager.primary,
+          width: AppSize.s1_5,
+        ),
+      ),
+      // error border
+      errorBorder: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s8),
+        ),
+        borderSide: BorderSide(
+          color: ColorManager.error,
+          width: AppSize.s1_5,
+        ),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s8),
+        ),
+        borderSide: BorderSide(
+          color: ColorManager.primary,
+          width: AppSize.s1_5,
+        ),
+      ),
+    ),
+  );
+}
